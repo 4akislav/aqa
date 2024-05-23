d33e8f5db33e60153b737045732beeacf6fc9a0d
4akislav <andron2588@gmail.com>
2024-05-23 14:07:31 +0300

    feat: checking inv_container added

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,14 +2,13 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('Login test', () => {
+    it('test', async () => {
         await LoginPage.open()
 
         await LoginPage.login('standard_user', 'secret_sauce')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!')
+        await expect(SecurePage.flashAlert).toExist()
+        
     })
 })
 
