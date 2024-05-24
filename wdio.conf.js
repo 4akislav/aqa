5b84ca7af3696fd3bf1019ca1c626a1e416ce87f
4akislav <andron2588@gmail.com>
2024-05-24 12:31:11 +0300

    feat: allure reported added

--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,12 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','junit'],
+    reporters: ['spec',
+    [
+        'allure',{
+            outDir: 'allure-results'}
+        
+    ]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
